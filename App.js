762fff7aae7119a89fbbcc475a698fe0e28c001a
erik <erik_g@web.de>
2022-09-24 19:37:18 +0300

    Assignment9

--- App.js
+++ App.js
@@ -1,11 +1,85 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { StyleSheet, View, Button, TextInput, Dimensions } from 'react-native';
+import { useState, useEffect } from 'react';
+import MapView, { Marker } from 'react-native-maps';
+import * as Location from 'expo-location';
+
 
 export default function App() {
+
+  const key = 'k1tDgHbYqlAa4QzK1TOMJG6nNxD0mmCp';
+
+  const [address, setAddress] = useState('');
+
+  const [data, setData] = useState('');
+
+  const [position, setPosition] = useState({
+    latitude: 60.200692,
+    longitude: 24.934302
+  });
+
+  const [location, setLocation] = useState(null);
+
+  useEffect(() => {
+
+    (async () => {
+      let { status } = await Location.requestForegroundPermissionsAsync();
+      if (status !== 'granted') {
+        Alert.alert('No permission to get location')
+        return;
+      }
+      let location = await Location.getCurrentPositionAsync({});
+      setLocation(location);
+
+      setPosition({
+        latitude: location.coords.latitude,
+        longitude: location.coords.longitude
+      })
+
+    })();
+    
+  }, []);
+
+  const fetchRepository = () => {
+
+    fetch('http://www.mapquestapi.com/geocoding/v1/address?key=' + key + '&location=' + address)
+      .then(response => response.json())
+      .then(data => setData((((data.results)[0].locations)[0].latLng)))
+      .catch(err => Alert.alert('Error', err));
+
+    setPosition({
+      latitude: data.lat,
+      longitude: data.lng
+    });
+
+  }
+
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
+
+      <MapView
+        style={styles.map}
+        region={{
+          ...position,
+          latitudeDelta: 0.01,
+          longitudeDelta: 0.01
+        }}>
+        <Marker
+          coordinate={{ ...position }}
+          title={address}
+        />
+      </MapView>
+
+      <View style={{ flex: 1 }}>
+        <TextInput
+          style={styles.textInput}
+          placeholder='Type in your address'
+          onChangeText={(address) => setAddress(address)}
+        />
+        <View style={styles.buttonView}>
+          <Button title='SHOW' onPress={fetchRepository} />
+        </View>
+      </View>
+
     </View>
   );
 }
@@ -17,4 +91,18 @@ const styles = StyleSheet.create({
     alignItems: 'center',
     justifyContent: 'center',
   },
+  textInput: {
+    width: Dimensions.get('window').width,
+    height: 40,
+    borderColor: 'gray',
+    borderWidth: 1
+  },
+  buttonView: {
+    width: Dimensions.get('window').width,
+  },
+  map: {
+    flex: 6,
+    width: Dimensions.get('window').width,
+    height: Dimensions.get('window').height
+  }
 });
